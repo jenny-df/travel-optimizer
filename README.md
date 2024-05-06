7111200a60fb4b22b640f5f50a45d0397e5168f1
Jennifer Fairhurst <jennifer.dakkak@gmail.com>
2024-05-05 23:28:32 -0400

    small changes

--- README.md
+++ README.md
@@ -13,15 +13,24 @@
   - Create a .gitignore file (if it doesn't exist) and add two lines to it:
     - env/
     - config.py
+    - .env
+  - Create a .env file:
+    - GOOGLE_MAPS_API_KEY="<KEY>"
 - Start the website: `python3 app.py`
 - Open the website (hosted locally). The link should be http://127.0.0.1:5000 or http://localhost:5000. The terminal will tell you which one it is.
-- If you make any change, it will be reflected on the website when you refresh the page. If you made a change in the CSS or JS code, it might not be reflected immediately because of cookies and cashing; you'll have to do a hard refresh of the page or open it from another browser to see the changes.
+- If you make any change, it will be reflected on the website when you refresh the page. If you made a change in the CSS or JS code, it might not be reflected immediately because of cookies and caching; you'll have to do a hard refresh of the page or open it from another browser to see the changes.
 
 ## Sidenotes:
 
 1. If you make a change to the configurations, please add the generic name of the variable above.
 2. If any major change is made please update the README.
 3. If you pip install any packages that everyone should have, make sure only relevant stuff is downloaded (AND everything currently in the requirements.txt file) and then run pip freeze > requirements.txt to reflect the changes for everyone.
+
+## Assumptions:
+
+- Users have 15 hours in a day available
+- Users spend one hour in each location
+- No flights between locations
 
 ## Todo List:
 
--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, request, url_for, redirect
+import os
+from dotenv import load_dotenv
+from data_scraper import *
+
 
 app = Flask(__name__,
 	template_folder='templates', 
 	static_folder='static'
 )
 
-
 app.config.from_pyfile('config.py')
 
+load_dotenv()
 
 @app.route('/', methods = ["GET"])
 def home():
@@ -16,8 +20,9 @@ def home():
 
 @app.route('/data', methods = ["GET"])
 def data():
-    google_key = "AIzaSyATeWwg3KcuetRkm1nV3xuOGAAZ6uXJNIU"
-    return render_template("data.html", google_key=google_key)
+    # <TODO> Tamar: Get all categories
+    categories = ['Parks', 'Casinos', 'Tourist Attractions']
+    return render_template("data.html", categories=categories, google_key=os.getenv('GOOGLE_MAPS_API_KEY'))
 
 
 @app.route('/results', methods = ["POST"])
@@ -25,17 +30,29 @@ def results():
     # Getting values from the form
     data = dict(request.form)
     del data['Submit']
-    data['locations'] = data['locations'].split('\n')
+    data['must_locations'] = data['must_locations'].split('\r\n')
+    data['categories_of_locations'] = request.form.getlist('categories_of_locations')
 
-	# <TODO> Call to classifier 
+	# <TODO> Audrey: Call to classifier 
     clustering_output = [["1.1", "1.2", "1.3"], ["2.1", "2.2"], ["3.1"]]
-    # <TODO> Call to optimized routing
+    # <TODO> Audrey: Call to optimized routing
     optimized_route_output = [["1.1", "2.1", "3.1", "2.2"], ["1.2", "1.3"]]
 
     return render_template("results.html", 
                         clusters = clustering_output, 
                         routes = optimized_route_output
                         )
+
+@app.route('/scrape/<string:city_name>', methods = ["POST"])
+def scrape(city_name):
+    # CHANGE TO GET if you want to use this!
+    try:
+        info = get_tourist_attractions(city_name)
+        print(info)
+    except:
+        print("ERROR in scraping")
+    
+    return redirect(url_for('home', _method="GET"))
 
 
 @app.route('/<path:other_path>', methods=['GET', 'POST'])
@@ -44,5 +61,4 @@ def catch_all(other_path):
 
 
 if __name__ == '__main__':
-	app.secret_key = 'sjkdfh239G$FJLD239#89QEFAS239DFJISALFJ!$'
 	app.run(debug = True)
