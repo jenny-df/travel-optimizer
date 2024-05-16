378233ad7cb198cbbeae990692491647fd9c424d
Audrey Lorvo <audreylorvo@dhcp-10-29-128-58.dyn.MIT.EDU>
2024-05-15 22:25:19 -0400

    merge

--- routing_basic.py
+++ routing_basic.py
@@ -65,7 +65,7 @@ def compute_time_matrix(transport_mode, distance_matrix):
     assert(transport_mode in TRANSPORT_SPEEDS, f"You are providing an invalid transport method {transport_mode}")
     return [[int(TRANSPORT_SPEEDS[transport_mode]*length) for length in distance] for distance in distance_matrix]
 
-def return_solution(data, manager, routing, solution, reference_list):
+# def return_solution(data, manager, routing, solution, reference_list):
     '''
     Finds the final output of the problem
     '''
@@ -107,6 +107,33 @@ def return_solution(data, manager, routing, solution, reference_list):
         plan.append(day_plan)
 
     return plan, total_travel_time, total_visit_time, len(sites)-1
+
+# Prints solution in Terminal
+def return_solution(data, manager, routing, solution):
+    print(f"Objective (distance/time travelled which we are minimizing): {solution.ObjectiveValue()}")
+    time_dimension = routing.GetDimensionOrDie("Time")
+    total_time = 0
+    for vehicle_id in range(data["num_vehicles"]):
+        index = routing.Start(vehicle_id)
+        plan_output = f"Route for vehicle {vehicle_id}:\n"
+        while not routing.IsEnd(index):
+            time_var = time_dimension.CumulVar(index)
+            plan_output += (
+                f"{manager.IndexToNode(index)}"
+                f" Time({solution.Min(time_var)},{solution.Max(time_var)})"
+                " -> "
+            )
+            index = solution.Value(routing.NextVar(index))
+        time_var = time_dimension.CumulVar(index)
+        plan_output += (
+            f"{manager.IndexToNode(index)}"
+            f" Time({solution.Min(time_var)},{solution.Max(time_var)})\n"
+        )
+        plan_output += f"Time of the route: {solution.Min(time_var)}min\n"
+        print(plan_output)
+        total_time += solution.Min(time_var)
+    print(f"Total time of all routes: {total_time}min")
+
 
 def create_time_callback(manager, time_matrix):
     def time_callback(from_i, to_i):
@@ -247,10 +274,12 @@ def router(required, optional, ranking_considered, transport_mode, days_traveled
     return [], 0, 0, 0
 
 if __name__ == '__main__':
+
+
     locations = [('HOTEL', 'Marriott Hotel', 42.3629114, -71.0861978, 540, 1080, 0), ('ChIJpbiA_0J344kRmiVu-fjcbAA', 'Massachusetts Hall', 42.3744368, -71.118281, 540, 1020, 60), ('ChIJP7WqWapw44kRiTw1teyTNdM', 'BLUE COVE MANAGEMENT, INC.', 42.360091, -71.0941599, 540, 1020, 60), ('ChIJa3g3jhBx44kRZPE5-nY3-gE', 'K-Curl Studio', 42.3548561, -71.0661193, 540, 1020, 60), ('ChIJbz8lP_Z544kRBFV6ZMsNgKI', 'Fenway Park', 42.3466764, -71.0972178, 540, 1020, 60), ('ChIJ7YKigxh644kR6D24lfwf8oA', 'Churchill Hall', 42.3387904, -71.088892, 420, 1140, 60), ('ChIJZRKlXXd644kRMqoHxDSSRD4', 'Chinatown', 42.3493259, -71.0621815, 540, 1020, 60)]
 
-# locations2 = [('ChIJpbiA_0J344kRmiVu-fjcbAA', 'Massachusetts Hall', 3, -71.118281, 540, 1020, 60), ('ChIJP7WqWapw44kRiTw1teyTNdM', 'BLUE COVE MANAGEMENT, INC.', 42.360091, -71.0941599, 540, 1020, 60), ('ChIJa3g3jhBx44kRZPE5-nY3-gE', 'K-Curl Studio', 42.3548561, -71.0661193, 540, 1020, 60), ('ChIJbz8lP_Z544kRBFV6ZMsNgKI', 'Fenway Park', 42.3466764, -71.0972178, 540, 1020, 60), ('ChIJ7YKigxh644kR6D24lfwf8oA', 'Churchill Hall', 42.3387904, -71.088892, 420, 1140, 60)]
+    locations2 = [('ChIJpbiA_0J344kRmiVu-fjcbAA', 'Massachusetts Hall', 42.3744368, -71.118281, 540, 1020, 60), ('ChIJP7WqWapw44kRiTw1teyTNdM', 'BLUE COVE MANAGEMENT, INC.', 42.360091, -71.0941599, 540, 1020, 60), ('ChIJa3g3jhBx44kRZPE5-nY3-gE', 'K-Curl Studio', 42.3548561, -71.0661193, 540, 1020, 60), ('ChIJbz8lP_Z544kRBFV6ZMsNgKI', 'Fenway Park', 42.3466764, -71.0972178, 540, 1020, 60), ('ChIJ7YKigxh644kR6D24lfwf8oA', 'Churchill Hall', 42.3387904, -71.088892, 420, 1140, 60)]
 
-#42.3744368
+    #42.3744368
 
-# print(router(locations2, [], True, 'car', 2))
+    print(router(locations2, [], True, 'car', 2))
